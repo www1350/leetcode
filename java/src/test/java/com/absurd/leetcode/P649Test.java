ea3a8397239a442fbe051d7bde4a13d7de208601
yunyong <yunyong.www@alibaba-inc.com>
2023-08-18 16:41:14 +0800

    comment

---
+++ java/src/test/java/com/absurd/leetcode/P649Test.java
@@ -0,0 +1,26 @@
+/*
+ * Ant Group
+ * Copyright (c) 2004-2023 All Rights Reserved.
+ */
+package com.absurd.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author yunyong.www
+ * @version P649Test.java, v 0.1 2023年08月18日 14:15 yunyong.www
+ */
+public class P649Test {
+    @Test
+    public void predictPartyVictory(){
+        P649 p649 = new P649();
+        Assert.assertEquals("Dire",p649.predictPartyVictory("DDRRR"));
+
+        Assert.assertEquals("Dire",p649.predictPartyVictory("DR"));
+
+        Assert.assertEquals("Radiant",p649.predictPartyVictory("DRR"));
+
+        Assert.assertEquals("Dire",p649.predictPartyVictory("RDD"));
+    }
+}
